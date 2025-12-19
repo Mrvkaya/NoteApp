569091d221154751ba0eac9b2bfd6dabcf2dc283
Merve <mrvky1689@gmail.com>
2025-12-19 23:19:55 +0300

    -Implemented a recent files system using a fixed-size array - Added persistent storage for recent files using a local text file - Implemented open, save, update and delete operations for notes - Ensured synchronization between the file system and the UI list - Added revert (cancel) functionality to restore the last saved content - Improved application state handling after file operations

--- NotUygulamasi/Form1.Designer.cs
+++ NotUygulamasi/Form1.Designer.cs
@@ -29,107 +29,130 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnAc = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnYeni = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lstDosyalar = new System.Windows.Forms.ListBox();
+            this.txtNotlar = new System.Windows.Forms.TextBox();
+            this.btnVazgec = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnVazgec);
+            this.panel1.Controls.Add(this.btnAc);
+            this.panel1.Controls.Add(this.btnSil);
+            this.panel1.Controls.Add(this.btnGuncelle);
+            this.panel1.Controls.Add(this.btnKaydet);
+            this.panel1.Controls.Add(this.btnYeni);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(672, 70);
+            this.panel1.Size = new System.Drawing.Size(955, 70);
             this.panel1.TabIndex = 0;
             // 
-            // button4
+            // btnAc
             // 
-            this.button4.Location = new System.Drawing.Point(513, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(131, 38);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "SİL";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.btnSil_Click);
+            this.btnAc.Location = new System.Drawing.Point(28, 12);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(131, 38);
+            this.btnAc.TabIndex = 4;
+            this.btnAc.Text = "AÇ";
+            this.btnAc.UseVisualStyleBackColor = true;
+            this.btnAc.Click += new System.EventHandler(this.btnAc_Click);
             // 
-            // button3
+            // btnSil
             // 
-            this.button3.Location = new System.Drawing.Point(362, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(131, 38);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "GÜNCELLE";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.btnGuncelle_Click);
+            this.btnSil.Location = new System.Drawing.Point(639, 12);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(131, 38);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
             // 
-            // button2
+            // btnGuncelle
             // 
-            this.button2.Location = new System.Drawing.Point(27, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 38);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "KAYDET";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.btnKaydet_Click);
+            this.btnGuncelle.Location = new System.Drawing.Point(482, 12);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(131, 38);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
             // 
-            // button1
+            // btnKaydet
             // 
-            this.button1.Location = new System.Drawing.Point(200, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 38);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "YENİ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnYeni_Click);
+            this.btnKaydet.Location = new System.Drawing.Point(332, 12);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(131, 38);
+            this.btnKaydet.TabIndex = 1;
+            this.btnKaydet.Text = "KAYDET";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.Location = new System.Drawing.Point(181, 12);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(131, 38);
+            this.btnYeni.TabIndex = 0;
+            this.btnYeni.Text = "YENİ";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            this.btnYeni.Click += new System.EventHandler(this.btnYeni_Click);
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.lstDosyalar);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 70);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 503);
+            this.panel2.Size = new System.Drawing.Size(200, 518);
             this.panel2.TabIndex = 1;
             // 
-            // listBox1
+            // lstDosyalar
             // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, 0);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(200, 503);
-            this.listBox1.TabIndex = 0;
+            this.lstDosyalar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lstDosyalar.FormattingEnabled = true;
+            this.lstDosyalar.Location = new System.Drawing.Point(0, 0);
+            this.lstDosyalar.Name = "lstDosyalar";
+            this.lstDosyalar.Size = new System.Drawing.Size(200, 518);
+            this.lstDosyalar.TabIndex = 0;
             // 
-            // textBox1
+            // txtNotlar
             // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(200, 70);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(472, 503);
-            this.textBox1.TabIndex = 2;
+            this.txtNotlar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtNotlar.Location = new System.Drawing.Point(200, 70);
+            this.txtNotlar.Multiline = true;
+            this.txtNotlar.Name = "txtNotlar";
+            this.txtNotlar.Size = new System.Drawing.Size(755, 518);
+            this.txtNotlar.TabIndex = 2;
+            // 
+            // btnVazgec
+            // 
+            this.btnVazgec.Location = new System.Drawing.Point(795, 12);
+            this.btnVazgec.Name = "btnVazgec";
+            this.btnVazgec.Size = new System.Drawing.Size(131, 38);
+            this.btnVazgec.TabIndex = 5;
+            this.btnVazgec.Text = "VAZGEÇ";
+            this.btnVazgec.UseVisualStyleBackColor = true;
+            this.btnVazgec.Click += new System.EventHandler(this.btnVazgec_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(672, 573);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(955, 588);
+            this.Controls.Add(this.txtNotlar);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
-         //   this.Load += new System.EventHandler(this.Form1_Load_1);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -140,13 +163,15 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnYeni;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox lstDosyalar;
+        private System.Windows.Forms.TextBox txtNotlar;
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.Button btnVazgec;
     }
 }
 
--- NotUygulamasi/Form1.cs
+++ NotUygulamasi/Form1.cs
@@ -1,143 +1,265 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace NotUygulamasi
 {
     public partial class Form1 : Form
     {
+        static int sonDosyalarSayisi = 20;
         string notesFolder = @"C:\DENEMENOTLAR";
         string seciliDosyaYolu = "";
+        string[,] sonDosyalar = new string[sonDosyalarSayisi, 2];
+        string sonDosyalarDosyasi = @"C:\DENEMENOTLAR\sondosyalar.txt"; //SON DOSYALAR DİZİSİNİN KALICI OLARAK KAYDEDİLECEĞİ DOSYA
 
         public Form1()
         {
             InitializeComponent();
             this.Load += Form1_Load;
-            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            lstDosyalar.SelectedIndexChanged += lstDosyalar_SelectedIndexChanged;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)  // FORMLOAD DA DOSYAYI OKUYUP DİZİYE KAYDETTİM
         {
-            if (!Directory.Exists(notesFolder))
-                Directory.CreateDirectory(notesFolder);
-
-            NotlariListele();
-        }
-
-        // NOTLARI LİSTELEME İŞLEMİ
-        private void NotlariListele()
-        {
-            var liste = Directory.GetFiles(notesFolder, "*.txt")
-                .Select(f =>
-                {
-                    string ilkSatir = File.ReadLines(f).FirstOrDefault();
-                    if (string.IsNullOrWhiteSpace(ilkSatir))
-                        ilkSatir = "(Başlıksız Not)";
-
-                    return new NoteItem
-                    {
-                        Title = ilkSatir,
-                        FilePath = f
-                    };
-                })
-                .OrderByDescending(x => x.FilePath)
-                .ToList();
-
-            listBox1.DataSource = null;
-            listBox1.DisplayMember = "Title";
-            listBox1.ValueMember = "FilePath";
-            listBox1.DataSource = liste;
-
-            if (!string.IsNullOrEmpty(seciliDosyaYolu))
+            try
             {
-                var secilecek = liste.FirstOrDefault(x => x.FilePath == seciliDosyaYolu);
-                if (secilecek != null)
-                    listBox1.SelectedItem = secilecek;
+                SonDosyalariYukle();
+                NotlariListele();
+                SecilenButonlariAyarla();
+            }
+            catch
+            {
+                MessageBox.Show("Notlar yüklenemedi.");
             }
         }
 
-        // NOTLARI OKUMA İŞLEMİ
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void SecilenButonlariAyarla()
         {
-            if (listBox1.SelectedItem == null) return;
-
-            var item = (NoteItem)listBox1.SelectedItem;
-            seciliDosyaYolu = item.FilePath;
-
-            if (File.Exists(seciliDosyaYolu))
-                textBox1.Text = File.ReadAllText(seciliDosyaYolu);
+            bool seciliVar = !string.IsNullOrEmpty(seciliDosyaYolu);
+            btnSil.Enabled = seciliVar;
+            btnGuncelle.Enabled = seciliVar;
+            btnYeni.Enabled = seciliVar;
+            btnKaydet.Enabled = !seciliVar;
+            btnVazgec.Enabled = !false;
         }
 
-        // YENİ NOT KAYDETME İŞLEMİ
+        // NOTLARI LİSTELE
+
+        private void NotlariListele()
+        {
+            lstDosyalar.Items.Clear();
+
+            for (int i = 0; i < sonDosyalarSayisi; i++)
+            {
+                if (!string.IsNullOrEmpty(sonDosyalar[i, 0]))
+                {
+                    lstDosyalar.Items.Add(sonDosyalar[i, 0]);
+                }
+            }
+        }
+
+
+        // LİSTEDEN SEÇ OKU
+
+        private void lstDosyalar_SelectedIndexChanged(object sender, EventArgs e) //SEÇİLİ OLAN DOSYA ADINI DİZİDE ARATIP ONUN YOLUNU SEÇİLİ DOSYA YOLUNA ATADIM
+        {
+            if (lstDosyalar.SelectedIndex == -1)
+                return;
+
+            int index = lstDosyalar.SelectedIndex;
+            seciliDosyaYolu = sonDosyalar[index, 1];
+
+            if (File.Exists(seciliDosyaYolu))
+                txtNotlar.Text = File.ReadAllText(seciliDosyaYolu);
+            else
+                txtNotlar.Clear();
+
+            SecilenButonlariAyarla();
+        }
+
+
+        // KAYDET
+
         private void btnKaydet_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            if (string.IsNullOrWhiteSpace(txtNotlar.Text))
             {
                 MessageBox.Show("Not boş olamaz");
                 return;
             }
 
-            if (!string.IsNullOrEmpty(seciliDosyaYolu))
-            {
-                MessageBox.Show("Bu not zaten var. Güncelle kullan.");
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.InitialDirectory = notesFolder;
+            dlg.Filter = "Text Files (*.txt)|*.txt";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
                 return;
-            }
 
-            string dosyaAdi = DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
-            seciliDosyaYolu = Path.Combine(notesFolder, dosyaAdi);
+            seciliDosyaYolu = dlg.FileName;
+            File.WriteAllText(seciliDosyaYolu, txtNotlar.Text);
 
-            File.WriteAllText(seciliDosyaYolu, textBox1.Text);
+            DosyayiUsteEkle(seciliDosyaYolu);
+
+            SonDosyalariKaydet();
+            //oncekiIcerik = txtNotlar.Text;
             NotlariListele();
+            lstDosyalar.SelectedIndex = 0;
+            SecilenButonlariAyarla();
         }
 
-        // GÜNCELLEME İŞLEMİ
+
+        // GÜNCELLE
+
         private void btnGuncelle_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(seciliDosyaYolu))
-            {
-                MessageBox.Show("Güncellenecek not seçmedin");
                 return;
-            }
 
-            if (string.IsNullOrWhiteSpace(textBox1.Text))
-            {
-                MessageBox.Show("Not boş olamaz");
-                return;
-            }
-
-            File.WriteAllText(seciliDosyaYolu, textBox1.Text);
-            NotlariListele();
+            File.WriteAllText(seciliDosyaYolu, txtNotlar.Text);
+            // oncekiIcerik = txtNotlar.Text;
+            MessageBox.Show("Not güncellendi");
         }
+
 
         // YENİ
+
         private void btnYeni_Click(object sender, EventArgs e)
         {
-            textBox1.Clear();
+            txtNotlar.Clear();
             seciliDosyaYolu = "";
-            listBox1.ClearSelected();
+            lstDosyalar.ClearSelected();
+            SecilenButonlariAyarla();
         }
 
+
         // SİLME İŞLEMİ
+
         private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (lstDosyalar.SelectedIndex == -1)
+                return;
+
+            int index = lstDosyalar.SelectedIndex;
+
+            if (File.Exists(sonDosyalar[index, 1]))
+                File.Delete(sonDosyalar[index, 1]);
+
+            for (int i = index; i < sonDosyalarSayisi; i++)
+            {
+                sonDosyalar[i, 0] = sonDosyalar[i + 1, 0];
+                sonDosyalar[i, 1] = sonDosyalar[i + 1, 1];
+            }
+
+            sonDosyalar[sonDosyalarSayisi - 1, 0] = "";
+            sonDosyalar[sonDosyalarSayisi - 1, 1] = "";
+
+            SonDosyalariKaydet();
+            NotlariListele();
+            txtNotlar.Clear();
+            seciliDosyaYolu = "";
+            SecilenButonlariAyarla();
+        }
+
+
+        // AÇ (HARİCİ TXT)
+
+        private void btnAc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Text Files (*.txt)|*.txt";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            seciliDosyaYolu = dlg.FileName;
+            txtNotlar.Text = File.ReadAllText(seciliDosyaYolu);
+
+
+            DosyayiUsteEkle(seciliDosyaYolu);
+
+            SonDosyalariKaydet();
+            NotlariListele();
+            lstDosyalar.SelectedIndex = 0;
+            SecilenButonlariAyarla();
+        }
+
+
+        // YARDIMCI METOTLAR
+
+        private void DosyayiUsteEkle(string dosyaYolu)
+        {
+            // varsa sil
+            for (int i = 0; i < sonDosyalarSayisi; i++)
+            {
+                if (sonDosyalar[i, 1] == dosyaYolu)
+                {
+                    for (int j = i; j < sonDosyalarSayisi - 1; j++)
+                    {
+                        sonDosyalar[j, 0] = sonDosyalar[j + 1, 0];
+                        sonDosyalar[j, 1] = sonDosyalar[j + 1, 1];
+                    }
+                    sonDosyalar[sonDosyalarSayisi - 1, 0] = "";
+                    sonDosyalar[sonDosyalarSayisi - 1, 1] = "";
+                    break;
+                }
+            }
+
+            // KAYDIR
+            for (int i = sonDosyalarSayisi-1; i > 0; i--)
+            {
+                if (sonDosyalar[i - 1, 0] != null)
+                {
+                    sonDosyalar[i, 0] = sonDosyalar[i - 1, 0];
+                    sonDosyalar[i, 1] = sonDosyalar[i - 1, 1];
+                }
+            }
+
+            sonDosyalar[0, 0] = Path.GetFileNameWithoutExtension(dosyaYolu);
+            sonDosyalar[0, 1] = dosyaYolu;
+        }
+
+        private void SonDosyalariKaydet() //SON DOSYALAR DİZİSİNİ BİR DOSYAYA KAYDETTİM
+        {
+            using (StreamWriter sw = new StreamWriter(sonDosyalarDosyasi))
+            {
+                for (int i = 0; i < sonDosyalarSayisi; i++)
+                {
+                    if (!string.IsNullOrEmpty(sonDosyalar[i, 0]))
+                        sw.WriteLine($"{sonDosyalar[i, 0]}|{sonDosyalar[i, 1]}");
+                }
+            }
+        }
+
+        private void SonDosyalariYukle()
+        {
+            if (!File.Exists(sonDosyalarDosyasi))
+                return;
+
+            var satirlar = File.ReadAllLines(sonDosyalarDosyasi);
+            int donguSayisi = Math.Min(satirlar.Length, sonDosyalarSayisi);
+
+            for (int i = 0; i < donguSayisi; i++)
+            {
+                string satir = satirlar[i];
+                string[] p = satir.Split('|');
+
+                if (p.Length == 2)
+                {
+                    sonDosyalar[i, 0] = p[0];
+                    sonDosyalar[i, 1] = p[1];
+                }
+            }
+        }
+
+        //VAZGEÇ
+        private void btnVazgec_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(seciliDosyaYolu))
             {
-                MessageBox.Show("Silinecek not seçmedin");
+                txtNotlar.Clear();
                 return;
             }
-
-            File.Delete(seciliDosyaYolu);
-
-            textBox1.Clear();
-            seciliDosyaYolu = "";
-            NotlariListele();
+            txtNotlar.Text = File.ReadAllText(seciliDosyaYolu);
         }
-    }
-
-    public class NoteItem
-    {
-        public string Title { get; set; }
-        public string FilePath { get; set; }
     }
 }
